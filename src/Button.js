5a35c6197df0557edcbcaa001a71795428c1b483
Rafia Ali <rafiaali0312@gmail.com>
2022-12-29 15:30:40 +0500

    style a button

---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Button() {
+  return (
+    <div className="App">
+     <button>Log in</button>
+    </div>
+  );
+}
+
+export default Button;
